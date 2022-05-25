df1c590d88549e1823923f3dd1f89ceb152505f5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-25 23:09:41 +0530

    imported pointers file to main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod vectors;
 mod conditionals;
 mod loops;
 mod functions;
+mod pointers;
 
 fn main() {
   print::run();
@@ -22,4 +23,5 @@ fn main() {
   conditionals::run();
   loops::run();
   functions::run();
+  pointers::run();
 }
